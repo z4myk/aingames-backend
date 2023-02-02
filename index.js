cc207cb8717f9a7629b4e3059119183308abaa29
Sebastian Mosquera <sebimosquera@gmail.com>
2023-02-02 09:58:49 -0300

    fix db connect

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
-const express = require('express')
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const cors = require('cors');
-
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
 
 
 
-app.listen(port || "9001", () => {
-    console.log('Servidor corriendo en puerto', port)
-})
+
+
+
+
+
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(process.env.MONGODB_LOCAL,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    })
+  .then(() => console.log("DB ONLINE"))
+  .catch((error) => console.log(error));
+
+app.listen(process.env.PORT || "9001", () => {  
+  console.log("Servidor corriendo en puerto", process.env.PORT);
+});
